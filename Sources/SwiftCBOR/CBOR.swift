4b9cd5f438aa9f322f86f88942a7b047d9bdcc64
Steven Marshall <steven@real-time-data.com.au>
2019-10-18 11:45:07 +1030

    Added default case

--- Sources/SwiftCBOR/CBOR.swift
+++ Sources/SwiftCBOR/CBOR.swift
@@ -110,6 +110,7 @@ public indirect enum CBOR : Equatable, Hashable,
         case (.float,       _): return false
         case (.double,      _): return false
         case (.break,       _): return false
+        case default: return false
         }
     }
 
